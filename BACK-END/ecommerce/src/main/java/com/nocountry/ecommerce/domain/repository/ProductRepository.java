4c0ac59cfa77dde47721b4c710ce0d78477a4410
Cristian Aguirre <cristianaaguirre0@gmail.com>
2022-05-24 11:39:34 -0300

    add findByName method to ProductRepository

--- BACK-END/ecommerce/src/main/java/com/nocountry/ecommerce/domain/repository/ProductRepository.java
+++ BACK-END/ecommerce/src/main/java/com/nocountry/ecommerce/domain/repository/ProductRepository.java
@@ -1,8 +1,10 @@
 package com.nocountry.ecommerce.domain.repository;
 
 import com.nocountry.ecommerce.domain.model.Product;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+   Product findByName(String name);
 }
