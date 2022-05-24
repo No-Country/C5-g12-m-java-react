eef236b74bef67178d457078faf19b676b2d7551
Andres Daniele <andresmdaniele@gmail.com>
2022-05-24 13:33:14 -0300

    feat: user services and it's implementation added

--- BACK-END/ecommerce/src/main/java/com/nocountry/ecommerce/domain/usecase/impl/UserServiceImpl.java
+++ BACK-END/ecommerce/src/main/java/com/nocountry/ecommerce/domain/usecase/impl/UserServiceImpl.java
@@ -1,16 +1,104 @@
 package com.nocountry.ecommerce.domain.usecase.impl;
 
+import com.nocountry.ecommerce.common.security.utils.JwtUtils;
+import com.nocountry.ecommerce.domain.model.User;
+import com.nocountry.ecommerce.domain.repository.RoleRepository;
+import com.nocountry.ecommerce.domain.repository.UserRepository;
 import com.nocountry.ecommerce.domain.usecase.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService, UserDetailsService {
+
+    @Lazy
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Lazy
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    private final static String ROLE_USER = "USER";
+
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        //Bean needed to implement UserDetailsService on JwtFilterRequest. Further implementation is needed
-        return null;
+    public User createUser(User user) {
+
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+            throw new RuntimeException("User with this email already exists");
+        }
+
+        user.setRole(roleRepository.findByName(ROLE_USER).orElseThrow((() -> new RuntimeException("Role not found"))));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        return userRepository.save(user);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return user;
+    }
+
+    @Override
+    public String login(String email, String password) {
+
+        Authentication auth = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(email, password));
+
+        UserDetails userDetails = (UserDetails) auth.getPrincipal();
+
+        return jwtUtils.generateToken(userDetails);
+    }
+
+    @Override
+    //update authenticated user
+    public User updateUser(Long id, User user) {
+        User userFromDb = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+
+        userFromDb.setFirstName(user.getFirstName());
+        userFromDb.setLastName(user.getLastName());
+        userFromDb.setPhone(user.getEmail());
+
+        if (user.getPassword() != null) {
+            userFromDb.setPassword(passwordEncoder.encode(user.getPassword()));
+        }
+
+        return userRepository.save(userFromDb);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+        userRepository.delete(user);
+    }
+
+    public User getMe(String jwt) {
+        return userRepository.findByEmail(jwtUtils.extractUsername(jwt)).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public boolean userMailAlreadyExists(String email) {
+        return userRepository.findByEmail(email).isPresent();
     }
 }
