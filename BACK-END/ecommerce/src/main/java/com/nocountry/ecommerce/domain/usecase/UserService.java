64de9048e8934987ea1adc9e865a8d60be03bd60
andresdaniele <85754013+andresdaniele@users.noreply.github.com>
2022-05-24 16:02:32 -0300

    Feature/user service (#21)
    * feat: user services and it's implementation added
    
    * feat: exceptions added

--- BACK-END/ecommerce/src/main/java/com/nocountry/ecommerce/domain/usecase/UserService.java
+++ BACK-END/ecommerce/src/main/java/com/nocountry/ecommerce/domain/usecase/UserService.java
@@ -1,4 +1,14 @@
 package com.nocountry.ecommerce.domain.usecase;
 
+import com.nocountry.ecommerce.domain.model.User;
+
 public interface UserService {
+    User createUser(User user);
+    String login (String email, String password);
+
+    User updateUser(Long id, User user);
+
+    void deleteUser(Long id);
+
+    User getMe(String jwt);
 }
