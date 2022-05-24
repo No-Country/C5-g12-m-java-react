b0c6910d9a81983bb92f907b0e5b7196bf8db46e
Andres Daniele <andresmdaniele@gmail.com>
2022-05-24 19:44:52 -0300

    fix: User SQL delete syntax error

--- BACK-END/ecommerce/src/main/java/com/nocountry/ecommerce/domain/model/User.java
+++ BACK-END/ecommerce/src/main/java/com/nocountry/ecommerce/domain/model/User.java
@@ -23,7 +23,7 @@ import java.util.Objects;
 @AllArgsConstructor
 @ToString
 @SQLDelete(sql = "UPDATE user SET is_active = false WHERE id = ?")
-@Where(clause = "is_active = false")
+@Where(clause = "is_active = true")
 public class User implements UserDetails {
 
     @Id
@@ -59,7 +59,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        if( this.role != null) {
+        if (this.role != null) {
             return Collections.singleton(this.role);
         }
         return Collections.emptySet();
