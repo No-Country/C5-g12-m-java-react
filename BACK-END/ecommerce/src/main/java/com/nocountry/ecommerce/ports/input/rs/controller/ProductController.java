d264c7d5a508f98bbd0f6b23a87d2496952359e8
Cristian Aguirre <cristianaaguirre0@gmail.com>
2022-05-25 22:45:20 -0300

    completing the functionalities of the controller

--- BACK-END/ecommerce/src/main/java/com/nocountry/ecommerce/ports/input/rs/controller/ProductController.java
+++ BACK-END/ecommerce/src/main/java/com/nocountry/ecommerce/ports/input/rs/controller/ProductController.java
@@ -1,15 +1,22 @@
 package com.nocountry.ecommerce.ports.input.rs.controller;
 
 import com.nocountry.ecommerce.domain.model.Product;
-import com.nocountry.ecommerce.domain.usecase.impl.IProductService;
+import com.nocountry.ecommerce.domain.usecase.impl.ProductServiceImpl;
+import com.nocountry.ecommerce.ports.input.rs.mapper.ProductMapper;
+import com.nocountry.ecommerce.ports.input.rs.request.CreateProductRequest;
+import com.nocountry.ecommerce.ports.input.rs.response.ProductDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+
+import java.net.URI;
+import java.util.List;
 
 import static com.nocountry.ecommerce.ports.input.rs.api.ApiConstants.PRODUCT_URI;
 
@@ -18,36 +25,43 @@ import static com.nocountry.ecommerce.ports.input.rs.api.ApiConstants.PRODUCT_UR
 @RequiredArgsConstructor
 public class ProductController {
 
-   private final IProductService service;
+   private final ProductServiceImpl service;
+   private final ProductMapper mapper;
 
    //====================Gets====================//
 
    @GetMapping
-   public ResponseEntity<?> getAllProducts() {
-      return ResponseEntity.ok(service.findAll());
+   public ResponseEntity<List<ProductDetails>> getAllProducts() {
+      return ResponseEntity.ok(
+         mapper.ListProductToProductDetailList(service.findAll())
+      );
    }
 
    //====================Posts====================//
 
-   @PostMapping(path = "create")
-   public ResponseEntity<?> createProduct(@RequestBody @Valid Product aux) throws Exception {
-      return ResponseEntity.ok().body(service.create(aux));
+   @PostMapping
+   public ResponseEntity<Void> createProduct(@RequestBody @Valid CreateProductRequest aux) {
+      long id = service.create(mapper.CreateProductToProduct(aux));
+      URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+         .path("{id}").buildAndExpand(id)
+         .toUri();
+      return ResponseEntity.created(location).build();
    }
 
    //====================Puts====================//
 
-   @PutMapping(path = "update/{id}")
-   public ResponseEntity<?> updateProduct(@PathVariable("id") @NotNull @Valid Long id,
-                                          @RequestBody @Valid Product aux) {
-      return ResponseEntity.ok().body(service.update(id, aux));
+
+   @PutMapping(path = "/{id}")
+   @ResponseStatus(HttpStatus.NO_CONTENT)
+   public void updateProduct(@PathVariable("id") @NotNull Long id, @RequestBody @Valid Product aux) {
+      service.update(id, aux);
    }
 
    //====================Deletes====================//
 
-   @DeleteMapping(path = "{id}")
+   @DeleteMapping(path = "/{id}")
    @ResponseStatus(HttpStatus.NO_CONTENT)
-   public ResponseEntity<?> deleteProduct(@PathVariable @NotBlank @Valid Long id) {
+   public void deleteProduct(@PathVariable @NotBlank @Valid Long id) {
       service.delete(id);
-      return ResponseEntity.ok().build();
    }
 }
