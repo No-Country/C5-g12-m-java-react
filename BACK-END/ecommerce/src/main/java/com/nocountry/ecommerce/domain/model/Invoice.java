9d68f0ca75c2c92737c9a90d1773f182e605617b
Joel Rodriguez <rgzjoel14@gmail.com>
2022-05-25 22:30:52 -0300

    scripts creates tables invoice&invoice_product

--- BACK-END/ecommerce/src/main/java/com/nocountry/ecommerce/domain/model/Invoice.java
+++ BACK-END/ecommerce/src/main/java/com/nocountry/ecommerce/domain/model/Invoice.java
@@ -1,12 +1,15 @@
 package com.nocountry.ecommerce.domain.model;
 
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
-@Table(name = "Invoice")
+@Table(name = "invoice")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -28,10 +31,30 @@ public class Invoice {
     @JoinTable(
             name="invoice_product",
             joinColumns=
-            @JoinColumn(name="invoice", referencedColumnName="invoice_id"),
+            @JoinColumn(name="invoice_id", referencedColumnName="invoice_id"),
             inverseJoinColumns=
-            @JoinColumn(name="product", referencedColumnName="product_id")
+            @JoinColumn(name="product_id", referencedColumnName="product_id")
     )
     private List<Product> productList;
 
+    @Column(name = "date", nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime creationDate;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Invoice invoice = (Invoice) o;
+
+        if (!id.equals(invoice.id)) return false;
+        if (!user.equals(invoice.user)) return false;
+        return productList.equals(invoice.productList);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
 }
