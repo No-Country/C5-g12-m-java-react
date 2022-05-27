4d308406382798f7627db67d1b675c18a81f2bb9
Joel Rodriguez <rgzjoel14@gmail.com>
2022-05-26 23:02:49 -0300

    refactor inserts fix

--- BACK-END/ecommerce/src/main/resources/db/migration/V8.2__insert_mark.sql
+++ BACK-END/ecommerce/src/main/resources/db/migration/V8.2__insert_mark.sql
@@ -2,7 +2,7 @@ INSERT INTO mark (mark_id, name)
 VALUES           (1, 'Logitech'),
                  (2, 'Genius'),
                  (3, 'Noganet'),
-                 (3, 'Redragon'),
-                 (4, 'Razer'),
-                 (5, 'Lenovo'),
-                 (6, 'Noga');
+                 (4, 'Redragon'),
+                 (5, 'Razer'),
+                 (6, 'Lenovo'),
+                 (7, 'Noga');
