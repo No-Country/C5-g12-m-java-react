9c01705f3b99e94f463571cb996cc64b1e47d44a
Leonardo Barrera <leonardobarrera397@gmail.com>
2022-05-30 12:58:49 -0300

    anotaciones @notBlank y @notEmpty

--- BACK-END/ecommerce/src/main/java/com/nocountry/ecommerce/ports/input/rs/request/MarkRequest.java
+++ BACK-END/ecommerce/src/main/java/com/nocountry/ecommerce/ports/input/rs/request/MarkRequest.java
@@ -1,5 +1,8 @@
 package com.nocountry.ecommerce.ports.input.rs.request;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +11,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MarkRequest {
+
+    @NotEmpty
+    @NotBlank
     private String name;
 }
