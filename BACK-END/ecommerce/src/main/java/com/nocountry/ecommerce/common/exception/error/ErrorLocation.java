dbc0a411bd9c478a6ce7e6098671137d9e488995
Joel Rodriguez <rgzjoel14@gmail.com>
2022-05-31 16:16:43 -0300

    changes for deploy

--- BACK-END/ecommerce/src/main/java/com/nocountry/ecommerce/common/exception/error/ErrorLocation.java
+++ BACK-END/ecommerce/src/main/java/com/nocountry/ecommerce/common/exception/error/ErrorLocation.java
@@ -1,13 +1,6 @@
 package com.nocountry.ecommerce.common.exception.error;
 
-<<<<<<< .merge_file_a06584
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-@AllArgsConstructor
-@Getter
-=======
->>>>>>> .merge_file_a11616
 public enum ErrorLocation {
     BODY, PATH, QUERY, HEADER
 }
