c3b9dfb496ad773f99c0cf007d1451210a90b79f
Leonardo Barrera <leonardobarrera397@gmail.com>
2022-06-02 21:09:12 -0300

    Category Service, impl and controller

--- BACK-END/ecommerce/src/main/java/com/nocountry/ecommerce/ports/input/rs/request/ProductUpdateRequest.java
+++ BACK-END/ecommerce/src/main/java/com/nocountry/ecommerce/ports/input/rs/request/ProductUpdateRequest.java
@@ -1,5 +1,8 @@
 package com.nocountry.ecommerce.ports.input.rs.request;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +11,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductUpdateRequest {
+   @NotEmpty
+   @NotBlank
    private String name;
+
+   @NotEmpty
    private Double price;
+
+   @NotEmpty
+   @NotBlank
    private String detail;
+
+   @NotEmpty
+   @NotBlank
    private String image;
 }
