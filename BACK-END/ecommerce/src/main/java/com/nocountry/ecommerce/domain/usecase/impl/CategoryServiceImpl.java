64d83ef5683b9fcc41015a8bc3afb505836077c0
Cristian Aguirre <cristianaaguirre0@gmail.com>
2022-06-04 13:22:36 -0300

    fixing exceptions

--- BACK-END/ecommerce/src/main/java/com/nocountry/ecommerce/domain/usecase/impl/CategoryServiceImpl.java
+++ BACK-END/ecommerce/src/main/java/com/nocountry/ecommerce/domain/usecase/impl/CategoryServiceImpl.java
@@ -1,29 +1,27 @@
 package com.nocountry.ecommerce.domain.usecase.impl;
 
-import java.util.List;
-
-import javax.transaction.Transactional;
-
-import org.springframework.stereotype.Service;
-
-import com.nocountry.ecommerce.common.exception.handler.AlreadyExistsException;
-import com.nocountry.ecommerce.common.exception.handler.NotFoundException;
+import com.nocountry.ecommerce.common.exception.error.ExistingNameException;
+import com.nocountry.ecommerce.common.exception.error.ResourceNotFoundException;
 import com.nocountry.ecommerce.domain.model.Category;
 import com.nocountry.ecommerce.domain.repository.CategoryRepository;
 import com.nocountry.ecommerce.domain.usecase.CategoryService;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
+import java.util.List;
+import javax.transaction.Transactional;
+
 @Service
+@RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
 
     private final CategoryRepository categoryRepository;
+    private final String NAME = "Category";
 
     @Override
     @Transactional
     public Category getByIdIfExists(Long id) {
-        return categoryRepository.findById(id).orElseThrow(() -> new NotFoundException(id));
+        return categoryRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(NAME,id));
     }
 
     @Override
@@ -32,35 +30,32 @@ public class CategoryServiceImpl implements CategoryService {
         return categoryRepository.findAll();
     }
 
-    @Override
     @Transactional
     public Long save(Category request) {
         existsName(request.getName());
         return categoryRepository.save(request).getId();
     }
 
-    @Override
     @Transactional
     public void update(Long id, Category request) {
-        Category category = categoryRepository.findById(id).orElseThrow(() -> new NotFoundException(id));
+        Category category = categoryRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(NAME,id));
 
         existsName(request.getName());
         category.setName(request.getName());
-
+        categoryRepository.save(category);
     }
 
-    @Override
     @Transactional
     public void deleteById(Long id) {
         Category category = categoryRepository.findById(id)
-                .orElseThrow(() -> new com.nocountry.ecommerce.common.exception.handler.ResourceNotFoundException(id));
+                .orElseThrow(() -> new ResourceNotFoundException(NAME,id));
         categoryRepository.deleteById(category.getId());
 
     }
 
     private void existsName(String name) {
         if (categoryRepository.findByName(name).isPresent())
-            throw new AlreadyExistsException("this name is already in use ");
+            throw new ExistingNameException(name);
     }
 
 }
