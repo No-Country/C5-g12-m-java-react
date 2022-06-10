bfa1e56b8aab3b0d750de5d69683fa85eb748d5e
Cristian Aguirre <90803789+Cristianaaguirre@users.noreply.github.com>
2022-06-09 23:22:44 -0300

    Feature/invoice implementation (#49)
    * create table resume and fix sql versions
    
    * changing class name and correcting its implementation
    
    * refactoring the purchase method and fixing sql tables
    
    * implement get Invoices method and create InvoiceResponse and SummaryResponse classes

--- BACK-END/ecommerce/src/main/java/com/nocountry/ecommerce/ports/input/rs/controller/InvoiceController.java
+++ BACK-END/ecommerce/src/main/java/com/nocountry/ecommerce/ports/input/rs/controller/InvoiceController.java
@@ -2,12 +2,14 @@ package com.nocountry.ecommerce.ports.input.rs.controller;
 
 import com.nocountry.ecommerce.domain.usecase.InvoiceService;
 import com.nocountry.ecommerce.ports.input.rs.api.ApiConstants;
+import com.nocountry.ecommerce.ports.input.rs.mapper.InvoiceMapper;
 import com.nocountry.ecommerce.ports.input.rs.request.PurchaseRequest;
+import com.nocountry.ecommerce.ports.input.rs.response.InvoiceResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,7 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class InvoiceController {
 
     private final InvoiceService invoiceService;
+    private final InvoiceMapper invoiceMapper;
 
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<List<InvoiceResponse>> getInvoices(@PathVariable("id") Long id) {
+        List<InvoiceResponse> responses = invoiceMapper.ListInvoiceToInvoiceResponse(invoiceService.getInvoices(id));
+        return ResponseEntity.ok(responses);
+    }
 
     @PostMapping
     public void processPurchase(@RequestBody PurchaseRequest request) {
